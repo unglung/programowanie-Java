01861fb9836dd2dabe7530aff149f150ba362fe0
unglung <151474450+unglung@users.noreply.github.com>
2025-11-20 12:22:37 +0100

    Add files via upload

---
+++ AM/lab07/app/src/main/java/com/example/lab07/DetailsActivity.java
@@ -0,0 +1,24 @@
+package com.example.lab07;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class DetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_details);
+
+        Button btnBack = findViewById(R.id.btnBack);
+
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish(); // wraca do poprzedniej aktywności
+            }
+        });
+    }
+}
